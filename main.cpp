bc794cbc5466d776078af80aacdc61d1ea0313a7
Igor <i.evdokimov@g.nsu.ru>
2025-09-29 21:59:45 +0700

    upload lab0a

--- main.cpp
+++ main.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
-#include "WordCounter.h"
+#include "module1.h"
+#include "module2.h"
+#include "module3.h"
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: word_count.exe input.txt output.csv\n";
-        return 1;
-    }
+using std::cout;
+using std::endl;
 
-    std::string inputFile = argv[1];
-    std::string outputFile = argv[2];
+int main(int argc, char** argv)
+{
+	std::cout <<  "Hello world!" << "\n";
 
-    WordCounter wc;
-    std::cout << "Reading and counting words...\n";
-    if (!wc.processFile(inputFile)) return 1;
+	// Явные вызовы
+	std::cout << Module1::getMyName() << "\n";
+	std::cout << Module2::getMyName() << "\n";
+	std::cout << Module3::getMyName() << "\n";
 
-    std::cout << "Saving CSV...\n";
-    if (!wc.saveCSV(outputFile)) return 1;
+	cout << "1"<< "\n";
 
-    std::cout << "Done! CSV saved to " << outputFile << "\n";
-    return 0;
+	// Исключение вероятности пересечения имён
+	cout << Module1::getMyName() << endl; 
+    cout << Module2::getMyName() << endl;
+    cout << Module3::getMyName() << endl;
+
+	cout << "2"<< "\n";
+
+	using namespace Module1; // (A) берётся функция из module1 / подключаются все имена
+	std::cout << getMyName() << "\n";
+
+	std::cout << Module2::getMyName() << "\n";
+
+	std::cout << Module3::getMyName() << "\n";
+
+	cout << "3"<< "\n";
+
+	//using namespace Module2; // (B)
+	//std::cout << getMyName() << "\n"; // COMPILATION ERROR (C)
+
+	//error: reference to 'getMyName' is ambiguous , тк getMyName есть и в module1 и module2
+
+	using Module2::getMyName; // (D) берётся только функция из module2
+	std::cout << getMyName() << "\n";
+
+	std::cout << Module1::getMyName() << "\n";
+
+	std::cout << Module3::getMyName() << "\n";
 }
